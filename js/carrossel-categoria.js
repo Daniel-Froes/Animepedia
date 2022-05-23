980121687d3e29133bfbc21db82da09851e6e452
Daniel Froes <danielfroestbf@gmail.com>
2022-05-23 17:52:45 -0300

    feat: arrumando bugs do carrossel

--- js/carrossel-categoria.js
+++ js/carrossel-categoria.js
@@ -7,14 +7,33 @@ gliders.forEach((glide) => {
   const gliderDots = document.querySelector(`#${glide.id} .dots`);
 
   new Glider(glider, {
-    slidesToShow: 5.5,
-    slidesToScroll: 1.5,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     draggable: true,
     dots: gliderDots,
     rewind:true,
     arrows: {
       prev: gliderPrev,
       next: gliderNext
-    }
+    },
+    responsive: [
+        {
+          breakpoint: 775,
+          settings: {
+            slidesToShow: 'auto',
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+          }
+        },{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6.5,
+            slidesToScroll: 1.5,
+            itemWidth: 150,
+            duration: 0.8
+          }
+        }
+      ]
   });
 });
