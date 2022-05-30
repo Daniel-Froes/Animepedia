1c1d0f5aed768dd22a3f9948d8a353c0e7bbe139
Daniel Froes <danielfroestbf@gmail.com>
2022-05-30 12:10:52 -0300

    feat: arrumando gulp e configurando arquivo bundle css

---
+++ Back-end/gulpfile.js
@@ -0,0 +1,24 @@
+const {src, dest, watch} = require('gulp');
+const compileSass = require('gulp-sass')(require('sass'));
+const minifyCss = require('gulp-clean-css');
+const sourceMaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat')
+
+compileSass.compiler = require('node-sass')
+
+const bundleSass = () => {
+    return src('./public/sass/**/*.scss')
+    .pipe(sourceMaps.init())
+        .pipe(compileSass().on('error', compileSass.logError))
+        .pipe(minifyCss())
+        .pipe(sourceMaps.write())
+        .pipe(concat('bundle.css'))
+        .pipe(dest('./public/stylesheets'))
+}
+
+const devWatch = () =>{
+    watch('./public/sass/**/*.scss', bundleSass)
+}
+
+exports.bundleSass = bundleSass;
+exports.devWatch = devWatch;
