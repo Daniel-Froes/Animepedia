3225a1b758279f0c31d0a8c08c7dc8221c8d951c
Daniel Froes <danielfroestbf@gmail.com>
2022-07-01 18:14:27 -0300

    feat: arrumando font family

--- animepedia/src/pages/index.tsx
+++ animepedia/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
 import "bootstrap/dist/css/bootstrap.css";
-
 import Header from '../ui/components/header/header';
+import Footer from '../ui/components/footer/footer';
 
 
 const Home: NextPage = () => {
   return (
    <>
     <Header/>
+    <Footer/>
    </>
   )
 }
