fa23bc4abfbf189511c048ecf707dba7b79c5a10
Daniel Froes <danielfroestbf@gmail.com>
2022-07-01 17:58:44 -0300

    feat: arrumando o header component

--- animepedia/src/pages/index.tsx
+++ animepedia/src/pages/index.tsx
@@ -1,34 +1,13 @@
 import type { NextPage } from 'next'
 import "bootstrap/dist/css/bootstrap.css";
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+import Header from '../ui/components/header/header';
 
 
 const Home: NextPage = () => {
   return (
    <>
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Header/>
    </>
   )
 }
--- animepedia/src/ui/components/header/header.tsx
+++ animepedia/src/ui/components/header/header.tsx
@@ -1,13 +1,82 @@
-import type { NextPage } from 'next'
-import "bootstrap/dist/css/bootstrap.css";
+import { Fragment } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Image from "next/image";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
-
-const Header: NextPage = () => {
-    return (
-        <>
-        
-        </>
-    )
+function Header() {
+  return (
+    <>
+      <header>
+        <Navbar className="header " expand="lg">
+          <Container fluid>
+            <Image
+              className="img-nav"
+              src="/images/img-nav.png"
+              alt="Logotipo Animepedia"
+              width="100"
+              height="100"
+            />
+            <Navbar.Brand className="img-nav" href="/">
+              Animepedia
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <NavDropdown title="Anime" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Populares</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Lançamentos
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action5">Gêneros</NavDropdown.Item>
+                  <NavDropdown.Item href="#action6">Ver tudo</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Mangá" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Populares</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Lançamentos
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action5">Gêneros</NavDropdown.Item>
+                  <NavDropdown.Item href="#action6">Ver tudo</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#">Contato</Nav.Link>
+                <Nav.Link href="#">Comunidade</Nav.Link>
+              </Nav>
+              <Form className="d-flex me-auto ">
+                <Form.Control
+                  type="search"
+                  placeholder="Ex: Shingeki no kyojin"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Procurar</Button>
+                <DropdownButton
+                  variant="outline-secondary"
+                  title="Dropdown"
+                  id="input-group-dropdown-4"
+                  align="end"
+                >
+                  <Dropdown.Item href="#">Action</Dropdown.Item>
+                  <Dropdown.Item href="#">Another action</Dropdown.Item>
+                  <Dropdown.Item href="#">Something else here</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item href="#">Separated link</Dropdown.Item>
+                </DropdownButton>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    </>
+  );
 }
 
-export default Header
+export default Header;
