70592f1b771cf39fdf577fe4cddc6ca39c3451e8
Daniel Froes <danielfroestbf@gmail.com>
2022-07-02 13:14:14 -0300

    feat: adicionando pastas e exemplo no for

--- Back-end/controllers/indexController.js
+++ Back-end/controllers/indexController.js
@@ -1,5 +1,20 @@
+const posts = [
+    {
+        id: 1,
+        title: "post 1",
+    },
+    {
+        id: 2,
+        title: "post 2",
+    },
+]
+
+
 const controller = {
-    index: (req, res) => res.render('index')
+    index: (req, res) => res.render('index', {
+        title: "Olá mundo",
+        posts
+    })
 }
 
 module.exports = controller;
--- animepedia/src/ui/components/index/sliderIndex.tsx
+++ animepedia/src/ui/components/index/sliderIndex.tsx
@@ -4,40 +4,26 @@ function SliderBigIndex() {
   return (
     <main>
       <Carousel>
-        <Carousel.Item interval={1000}>
+        <Carousel.Item interval={5000}>
           <img
             className="d-block w-100 imagem-carrossel"
             src="/images/officialtrailer.jpg"
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        <Carousel.Item interval={5000}>
           <img
             className="d-block w-100 imagem-carrossel"
             src="/images/snk.jpg"
             alt="Second slide"
           />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        <Carousel.Item interval={5000}>
           <img
             className="d-block w-100 imagem-carrossel"
             src="images/chainsawman.jpg"
             alt="Third slide"
           />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </main>
