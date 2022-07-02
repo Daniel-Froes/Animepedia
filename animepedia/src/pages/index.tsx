d64a577dc214937f8cdd2f36464a6df874c2c093
Daniel Froes <danielfroestbf@gmail.com>
2022-07-02 11:41:47 -0300

    feat: arrumando slider

--- animepedia/src/pages/index.tsx
+++ animepedia/src/pages/index.tsx
@@ -1,19 +1,16 @@
 import type { NextPage } from 'next'
 import "bootstrap/dist/css/bootstrap.css";
-import Header from '../ui/components/header/header';
-import Footer from '../ui/components/footer/footer';
+import Index from '../ui/components/index';
 
-import Teste from '../ui/components/sliderCategory/sliderCategory';
-import SliderBigIndex from '../ui/components/sliderCategory/sliderIndex';
+
+
 
 
 const Home: NextPage = () => {
   return (
    <>
     
-    <Header/>
-    <SliderBigIndex/>
-    <Footer/>
+      <Index/>
 
    </>
   )
---
+++ animepedia/src/ui/components/index/sliderNews.tsx
@@ -0,0 +1,34 @@
+import Glider from "react-glider";
+import "glider-js/glider.min.css";
+import CardNews from "./card";
+
+export default function SliderNews() {
+  return (
+    <>
+      <div className="App">
+        <div className="container">
+          <Glider
+            hasArrows
+            slidesToShow={3}
+            slidesToScroll={3}
+            scrollLock
+            hasDots
+          >
+            <div className="slide"><CardNews/></div>
+            <div className="slide"><CardNews/></div>
+            <div className="slide"><CardNews/></div>
+            <div className="slide"><CardNews/></div>
+            <div className="slide"><CardNews/></div>
+            <div className="slide"><CardNews/></div>
+            <div className="slide">7</div>
+            <div className="slide">8</div>
+            <div className="slide">9</div>
+            <div className="slide">10</div>
+            <div className="slide">11</div>
+            <div className="slide">12</div>
+          </Glider>
+        </div>
+      </div>
+    </>
+  );
+}
