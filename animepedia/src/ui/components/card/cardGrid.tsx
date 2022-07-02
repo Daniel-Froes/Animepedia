5572812a3b9e57f6ae6bdf1a86258176a47598dd
Isabella Borges <isabellab.ferreira5@gmail.com>
2022-07-02 11:42:04 -0300

    feat: adicionando componente de cards

---
+++ animepedia/src/ui/components/card/cardGrid.tsx
@@ -0,0 +1,42 @@
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+function CardGrid() {
+  return (
+    <CardGroup>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" className=".imagem-card-grid" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" className=".imagem-card-grid" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" className=".imagem-card-grid" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardGroup>
+  );
+}
+
+export default CardGrid;
