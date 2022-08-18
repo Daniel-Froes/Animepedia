df4b1352ab4f7f2a862a611f21fd8aabb9ad229f
Isabella <isabellab.ferreira5@gmail.com>
2022-08-18 19:17:26 -0300

    feat: adicionando editar perfil

---
+++ animepedia/src/pages/[lc]/editar.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import "bootstrap/dist/css/bootstrap.css";
+import EditarPerfil from '../../ui/components/perfil/editarPerfil';
+
+
+
+
+const Editar: NextPage = () => {
+  return (
+   <>   
+      <EditarPerfil />
+   </>
+ 
+  )
+}
+
+export default Editar
